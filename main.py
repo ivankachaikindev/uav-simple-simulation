0d4df5972396827ac11fc65d32ca54c1e6f06ec3
ivankachaikindev <ivan.kachaikin.dev@gmail.com>
2023-12-21 18:05:42 +0200

    Implement first main that only checks drone connection

---
+++ main.py
@@ -0,0 +1,24 @@
+from argparse import ArgumentParser
+
+from dronekit import connect
+
+parser = ArgumentParser('Launch drone, move from A to B, then rotate')
+parser.add_argument(
+    '--drone_ip', type=str, required=True,
+    help='Drone IP to connect manipulate with'
+)
+
+
+def main(drone_ip):
+    drone = __connect_to_drone(drone_ip)
+    print(drone.mode.name)
+
+
+def __connect_to_drone(drone_ip):
+    drone = connect(drone_ip, wait_ready=True)
+    return drone
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    main(args.drone_ip)
